2de16f8f209f37d48bc526e3c5d153c18ec1ad14
AkashDotJS <akash06082006@gmail.com>
2025-09-18 14:34:26 +0530

    :package: init

---
+++ src/env.ts
@@ -0,0 +1,19 @@
+/** biome-ignore-all lint/style/noProcessEnv: _ */
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.url(),
+  },
+  client: {},
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  emptyStringAsUndefined: true,
+  onValidationError: (issues) => {
+    // biome-ignore lint/suspicious/noConsole: _
+    console.error(z.prettifyError({ issues }));
+    process.exit(1);
+  },
+});
